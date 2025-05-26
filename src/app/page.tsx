300aaa8359fd8f3366b4c00ecbbff12c84e923ec
Hamza Mukhtar <mukhtarhamza294@gmail.com>
2025-05-26 11:51:52 +0500

    02- customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
-  return <div><p className="text-rose-500">Hello World</p></div>
+  return (
+    <div className="space-y-4 p-4">
+      <Button variant={"elevated"}>I am Button</Button>
+      <Input placeholder="Input" />
+      <Progress value={50} />
+      <Textarea placeholder="Textarea"/>
+      <Checkbox />
+    </div>
+  );
 }
